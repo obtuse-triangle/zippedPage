cbc09a2657c265407769dab866edb70f2981c406
obtuse-triangle <me@obtuse.kr>
2023-10-02 07:36:27 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const pako = require("pako");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("*", (req, res) => {
+    res.send(gzipToText(req.path.slice(1)));
+});
+
+function gzipToText(input) {
+    try {
+        var stringValue = atob(input.trim());
+        var charArray = stringValue.split("").map(function (x) {
+            return x.charCodeAt(0);
+        });
+        return pako.ungzip(charArray, { to: "string" });
+    } catch (error) {
+        return "Value is not a valid GZIP compressed text.";
+    }
+}
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
