705d2b16e6dde103a55c7f6f72fffe4c22b1116b
obtuse-triangle <me@obtuse.kr>
2023-10-02 10:11:37 +0900

    brotli support

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const pako = require("pako");
 const express = require("express");
+const zlib = require("zlib");
 
 const app = express();
 const port = 3001;
 
 app.get("*", (req, res) => {
-    res.send(gzipToText(req.path.slice(1)));
+    var input = req.path.slice(1);
+    var decomp = new Promise((resolve, reject) => {
+        var compressedData = Buffer.from(input, "base64");
+        try {
+            var stringValue = compressedData.toString("binary");
+            var charArray = stringValue.split("").map(function (x) {
+                return x.charCodeAt(0);
+            });
+            resolve(pako.ungzip(charArray, { to: "string" }));
+        } catch (error) {
+            zlib.brotliDecompress(compressedData, (err, data) => {
+                if (err) {
+                    resolve("It's not compressd text");
+                } else {
+                    resolve(Buffer.from(data, "base64").toString("utf8"));
+                }
+            });
+        }
+    });
+    decomp.then((value) => {
+        res.send(value);
+    });
 });
 
-function gzipToText(input) {
-    try {
-        var stringValue = Buffer.from(input.trim(), "base64").toString("binary");
-        var charArray = stringValue.split("").map(function (x) {
-            return x.charCodeAt(0);
-        });
-        return pako.ungzip(charArray, { to: "string" });
-    } catch (error) {
-        return "Value is not a valid GZIP compressed text.";
-    }
-}
+function gzipToText(input) {}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
